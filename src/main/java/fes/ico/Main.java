86a81ddcf9c88c02665a39f8122dbd3a63aa05bc
Aksel <jonhaksel@gmail.com>
2023-02-18 21:17:03 -0600

    funcionando

--- src/main/java/fes/ico/Main.java
+++ src/main/java/fes/ico/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.print("Escribe tu Nombre, Profesion y Pais de origen: ");
         String[] entrada = teclado.nextLine().trim().split(" ");
         for (int i = 0; i < 3; i++) {
-            System.out.println("+----------------------+");
+            System.out.println("+-----------------------+");
             if (i == 0) {
                 System.out.println("    Nombre: " + entrada[i]);
             }
@@ -18,7 +18,7 @@ public class Main {
             if (i == 2){
                 System.out.println("    Pais: " + entrada[i]);
             }
-            System.out.println("+----------------------+");
+            System.out.println("+-----------------------+");
         }
     }
 }
