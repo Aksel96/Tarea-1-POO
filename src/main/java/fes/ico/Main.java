f04511d70aaa2ca4ad7d9548169a3b727107362f
Aksel <jonhaksel@gmail.com>
2023-02-19 20:37:06 -0600

    Comentarios y portada

--- src/main/java/fes/ico/Main.java
+++ src/main/java/fes/ico/Main.java
@@ -1,12 +1,27 @@
+/*
+################################
+#            UNAM              #
+#        FES ARAGON            #
+#            ICO               #
+#            POO               #
+#          TAREA 1             #
+#    AKSEL VILLELA ANDRADE     #
+################################
+
+### Programa que pide por teclado un numbre, profesion y pais, y lo regresa ordenado. ###
+
+*/
 package fes.ico;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Escribe tu Nombre, Profesion y Pais de origen: ");
-        String[] entrada = teclado.nextLine().trim().split(" ");
+        //Usamos scanner para pedir por teclado la cadena
+        Scanner teclado = new Scanner(System.in); // creamos la instancia teclado
+        System.out.print("Escribe tu Nombre, Profesion y Pais de origen: "); //Pedimos la cadena
+        String[] entrada = teclado.nextLine().trim().split(" "); //Asignamos la cadena a un areglo de stings, limpiamos basura con trim y lo rebanamos con split
+        //Ciblo for para imprimir la cadena ene l orden pedido
         for (int i = 0; i < 3; i++) {
             System.out.println("+-----------------------+");
             if (i == 0) {
