24f9651f9df1e3eb1979f4ffb2ee0245045eb477
Aksel <jonhaksel@gmail.com>
2023-02-19 21:19:03 -0600

    funcion para verificar si hay un numero en la cadena

--- src/main/java/fes/ico/Main.java
+++ src/main/java/fes/ico/Main.java
@@ -15,25 +15,51 @@ package fes.ico;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args){
         //Usamos scanner para pedir por teclado la cadena
         Scanner teclado = new Scanner(System.in); // creamos la instancia teclado
         System.out.print("Escribe tu Nombre, Profesion y Pais de origen: "); //Pedimos la cadena
-        String[] entrada = teclado.nextLine().trim().split(" "); //Asignamos la cadena a un areglo de stings, limpiamos basura con trim y lo rebanamos con split
-        //Ciblo for para imprimir la cadena ene l orden pedido
-        for (int i = 0; i < 3; i++) {
-            System.out.println("+-----------------------+");
-            if (i == 0) {
-                System.out.println("    Nombre: " + entrada[i]);
+        //Primero hacemos un split de toda la cadena para hacer verificacion de si tiene un numero
+        String entrada = teclado.nextLine().trim();
+        //Areglo de la cadena spliteada elemento por elemento para comprobar si hay un numero
+        String[] comprobacionEntrada = entrada.split("");
+        //Llamamos a la funcion de comprobacion, para ver si hay un numero en la cadena
+        boolean resultadoComprobacion = verificaNumero(comprobacionEntrada);
+        //Ciclo for para imprimir la cadena ene l orden pedido
+        if (!resultadoComprobacion){
+            //Creamos una cadena y la splitemos con los datos validos
+            String[] datosUsuario = entrada.trim().split(" ");
+            //Iteramos la cadena para imprimir la informacion
+            for (int i = 0; i < 3; i++) {
+                System.out.println("+-----------------------+");
+                if (i == 0) {
+                    System.out.println("    Nombre: " + datosUsuario[i].toUpperCase());
+                }
+                if (i == 1){
+                    System.out.println("    Profesion: " + datosUsuario[i].toUpperCase());
+                }
+                if (i == 2){
+                    System.out.println("    Pais: " + datosUsuario[i].toUpperCase());
+                }
+                System.out.println("+-----------------------+");
             }
-            if (i == 1){
-                System.out.println("    Profesion: " + entrada[i]);
-            }
-            if (i == 2){
-                System.out.println("    Pais: " + entrada[i]);
-            }
-            System.out.println("+-----------------------+");
+    }else {
+            System.out.println("\nCARACTERES NO VALIDOS, POR FAVOR INTRODUCE OTROS (!No se admiten Numeros¡\n");
         }
+    }
+    //Funcion para verificar si hay un numero en la cadena
+    public static boolean verificaNumero(String[] entrada){
+
+        for (String elemento:entrada){
+            try {
+                int compruebaNumero = Integer.parseInt(elemento);
+                    return true;
+
+            }catch (NumberFormatException e){//Si no puede continuea con el nucle
+            }
+        }
+        return false;
     }
 }
